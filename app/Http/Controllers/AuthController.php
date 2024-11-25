0c1a088279631fe7c6d43ac0afc0108983844cdf
NasohDH <www.super.nasoh.new@gmail.com>
2024-11-25 17:12:46 +0300

    Added signup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -38,7 +40,54 @@ class AuthController extends Controller
     public function logout(Request $request){
         $request->user()->tokens()->delete();
         return response()->json([
-                'messages'=>'logged out successfully'
+            'messages'=>'logged out successfully'
         ],200);
+    }
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all() , [
+            'phone' => ['required','max:10','min:10'],
+            'password' => ['required','min:8'],
+            'code' => ['required','min:6','max:6'],
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'message' => "Register failed",
+                'data' =>$validator->errors()
+            ]);
+        }
+        $phone = $request->input('phone');
+        $password = $request->input('password');
+        $code = $request->input('code');
+        $sms = Message::where('phone',$phone)->latest()->first();
+        if(!$sms)
+            return response()->json (['message' => 'Incorrect code'], 400);
+        if($sms['code']!=$code)
+            return response()->json (['message' => 'Incorrect code'], 400);
+        User::create([
+            'phone'=>$phone,
+            'password'=>Hash::make($password),
+            'phone_verified_at'=>Carbon::now()
+        ]);
+        return response(['message' => 'User created successfully']);
+    }
+    public function checkPhoneAndPassword(Request $request)
+    {
+        $validator = Validator::make($request->all() , [
+            'phone' => ['required','max:10','min:10'],
+            'password' => ['required','min:8'],
+        ]);
+        if ($validator->fails()){
+            return response()->json([
+                'message' => "Invalid input",
+                'data' =>$validator->errors()
+            ]);
+        }
+        $phone = $request->input('phone');
+        if(User::query()->where('phone', $phone)->first())
+            return response()->json (['message' => 'The number is already in use'], 409);
+        if($phone[0]!=0||$phone[1]!=9)
+            return response()->json (['message' => 'Number should start with 09'], 400);
+        return response()->json (['message' => 'Done']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MessageController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,8 +11,14 @@ use Illuminate\Support\Facades\Route;
 //})->middleware('auth:sanctum');
 
 Route::controller(AuthController::class)->group(function (){
-//    Route::post('/register' , 'register');
-    Route::post('/login' , 'login');
-    Route::post('/logout' , 'logout')->middleware('auth:sanctum');
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout')->middleware('auth:sanctum');
+    Route::post('/checkPhoneAndPassword', 'checkPhoneAndPassword');
+});
+Route::controller(UserController::class)->group(function (){
+    Route::post('/completeUserInfo','completeUserInfo');
+    Route::get('/getUserInfo/{phone}', 'getUserInfo');
+    Route::get('/getUserImage/{phone}', 'getUserImage');
 });
 Route::post('/sendMessage', [MessageController::class, 'sendMessage']);
