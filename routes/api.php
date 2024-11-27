76d1d757f9e8031bedd58cd52a1d99044f20c7c8
Abdulrahman Othman <abdulrahman.othamn.sy@gmail.com>
2024-11-27 07:14:54 +0200

    implement login logic

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 //})->middleware('auth:sanctum');
 
 Route::controller(AuthController::class)->group(function (){
-//    Route::post('/register' , 'register');
+    //Route::post('/register' , 'register');
     Route::post('/login' , 'login');
     Route::post('/logout' , 'logout')->middleware('auth:sanctum');
 });
