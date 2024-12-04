fbb59a9d3d852705188df9d129362444cd1b88b0
NasohDH <www.super.nasoh.new@gmail.com>
2024-12-04 19:26:37 +0300

    Added products and stores logic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CategoriesTableSeeder::class,
-            UsersTableSeeder::class,
-            StoresTableSeeder::class,
-            ProductsTableSeeder::class,
-            ProductCategoryTableSeeder::class,
+//            CategoriesTableSeeder::class,
+//            UsersTableSeeder::class,
+//            StoreSeeder::class,
+            ProductSeeder::class,
+//            ProductCategoryTableSeeder::class,
         ]);
     }
 }
