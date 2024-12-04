5ac2141ebc6ec7ef7a608ea5ad9397777780dfbf
NasohDH <www.super.nasoh.new@gmail.com>
2024-12-04 23:54:35 +0300

    Improves

--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -6,10 +6,10 @@ use App\Models\Product;
 use App\Models\Store;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\StoreSeeder;
+use Database\Seeders\StoresTableSeeder;
 use Illuminate\Support\Facades\DB;
 
-class ProductSeeder extends Seeder
+class ProductsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
--- database/seeders/StoresTableSeeder.php
+++ database/seeders/StoresTableSeeder.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class StoreSeeder extends Seeder
+class StoresTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
